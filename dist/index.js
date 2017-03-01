070ae8aa107f08b63758b251cadd4c3f65279dd7
jeff <jeff>
2017-02-28 22:44:15 -0800

    logic fix

--- dist/index.js
+++ dist/index.js
@@ -107,7 +107,7 @@ var AnimateOnChange = function (_Component) {
         this.removeEventListeners('endRemoved', this.refs.root, this.animationStart);
       }
       // send separate, animation state change will not render
-      if (this.props.forwards) {
+      if (!this.props.forwards) {
           this.setState({ clearAnimationClass: true }); // renders
       }
       this.setState({ animating: false, clearAnimationClass: false });
